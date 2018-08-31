642e601bc947902315f282326b2a089a2d80f63b
Steven Brus <sbrus@lanl.gov>
2018-08-31 16:44:46 -0600

    Make switch.py callable from another directory
     - Create the switch file in the directory that the script is
       called from

--- switch.py
+++ switch.py
@@ -1,8 +1,11 @@
+import os
 import define_switches 
+
+pwd = os.getcwd()
 
 switches = define_switches.switch
 
-f = open('switch','w')
+f = open(pwd+'/switch','w')
 
 for switch in switches:
     if switches[switch][0] == True:
