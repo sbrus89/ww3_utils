f03a6dc67c76ead4962f8444fa889873f5900e47
Steven Brus <sbrus@lanl.gov>
2018-09-06 16:28:05 -0600

    Add stations.py and activestations.xml
     - stations.py parses activestations.xml and creates a list of
       stations to be written to the ww3_shel.inp file
    
     - The stations file is written by ww3shel.py and specified in
       define_shel.py

---
+++ stations/stations.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET
+import os
+
+pwd = os.getcwd()
+fdir = os.path.dirname(os.path.realpath(__file__))
+
+
+tree = ET.parse(fdir+'/activestations.xml')
+root = tree.getroot()
+
+owners = ['NDBC']
+
+f = open(pwd+'/stations.txt','w')
+for sta in root:
+  for owner in owners:
+    if sta.attrib['owner'] == owner:
+      lon = str(float(sta.attrib['lon']) + 180.0)
+      lat = sta.attrib['lat']
+      name = "'"+sta.attrib['id']+"'"
+      prgm = sta.attrib['pgm']      
+      f.write('  '.join([lon,lat,name,owner,prgm])+'\n')
+
+
+
