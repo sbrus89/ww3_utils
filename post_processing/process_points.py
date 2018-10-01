0d3b9eb4a922feb9ed6e2dee5aa464d135ea5ef0
Steven Brus <sbrus@lanl.gov>
2018-10-01 10:57:20 -0600

    Improvements to process_points.py
     - Add ablity to process multiiple output types, i.e. wind and wave
       data.

--- post_processing/process_points.py
+++ post_processing/process_points.py
@@ -3,43 +3,35 @@ import os
 import glob
 
 # Set directories
-output_direc = '/lustre/scratch1/turquoise/sbrus/WW3_testing/glo_15m/' 
+run_direc = '/lustre/scratch1/turquoise/sbrus/WW3_testing/glo_15m/' 
+output_direc = '/lustre/scratch1/turquoise/sbrus/WW3_testing/glo_15m/post_processing/model_output/' 
 pwd = os.getcwd()
 
-# Link the mod_def.ww3 file to the current directory
-subprocess.call(['ln','-sf',output_direc+'mod_def.ww3',pwd])
+# Output type options
+out_types = [{'type':'2','prefix':'ww3.','subtype':'2'},  # mean wave parameters 
+             {'type':'2','prefix':'cfsr.','subtype':'1'}] # depth, current, wind
 
-# Loop over all out_pnt.ww3.YYYYMMDD_HHMMSS-YYMMDD_HHMMSS files
-pnt_files = sorted(glob.glob(output_direc+'out_pnt.ww3.*'))
-for f in pnt_files:
+###################################################################################################
+###################################################################################################
 
-  # Link the out_pnt.ww3 file to the current directory
-  subprocess.call(['ln','-sf',f,pwd+'/out_pnt.ww3'])
+def replace_ww3_ounp_inp_line(nline,opt1,opt2=None):
 
-  # Find the start and end dates from the filename
-  date_range = f.split(".")[-1]  
-  start_date_time = date_range.split('-')[0]
-  start_date = start_date_time.split('_')[0]
-  start_time = start_date_time.split('_')[1]
-  print start_date,start_time
-
-  # Check if the ww3_ounp input file exists
-  if not os.path.isfile(pwd+'/ww3_ounp.inp'):
-    print 'ww3_ounp.inp not found'
-    raise SystemExit(0)
-
-  # Find the time information line in the ww3_ounp input file
+  # Find the requested line (nline) in the ww3_ounp input file
   founp = open(pwd+'/ww3_ounp.inp','r')
   lines = founp.read().splitlines()
+  line_count = 0
   for n,line in enumerate(lines):
     if line.strip()[0] != '$':
+      line_count = line_count + 1
+    if line_count == nline:
       break
   
-  # Replace the start date and time to match the out_pnt.ww3 file
-  time_info = lines[n].split()
-  time_info[0] = start_date
-  time_info[1] = start_time
-  lines[n] = '   '+'  '.join(time_info)
+  # Replace the line with the new information (opt1 and opt2) 
+  line_info = lines[n].split()
+  line_info[0] = opt1
+  if opt2:
+    line_info[1] = opt2
+  lines[n] = '   '+'  '.join(line_info)
   founp.close()
   
   # Re-write the ww3_ounp input file
@@ -47,6 +39,43 @@ for f in pnt_files:
   founp.write('\n'.join(lines))
   founp.close()
 
-  # Run the ww3_ounp program
-  subprocess.call([pwd+'/ww3_ounp'])
+###################################################################################################
+###################################################################################################
 
+if __name__ == '__main__':
+
+  # Check if the ww3_ounp input file exists
+  if not os.path.isfile(pwd+'/ww3_ounp.inp'):
+    print 'ww3_ounp.inp not found'
+    raise SystemExit(0)
+  
+  # Link the mod_def.ww3 file to the current directory
+  subprocess.call(['ln','-sf',run_direc+'mod_def.ww3',pwd])
+  
+  # Loop over all out_pnt.ww3.YYYYMMDD_HHMMSS-YYMMDD_HHMMSS files
+  pnt_files = sorted(glob.glob(output_direc+'out_pnt.ww3.*'))
+  for f in pnt_files:
+  
+    # Link the out_pnt.ww3 file to the current directory
+    subprocess.call(['ln','-sf',f,pwd+'/out_pnt.ww3'])
+  
+    # Find the start and end dates from the filename
+    date_range = f.split(".")[-1]  
+    start_date_time = date_range.split('-')[0]
+    start_date = start_date_time.split('_')[0]
+    start_time = start_date_time.split('_')[1]
+    print start_date,start_time
+  
+    # Replace the time information line
+    replace_ww3_ounp_inp_line(1,start_date,start_time)
+
+    for otype in out_types:
+      
+      # Replace the output type information lines
+      replace_ww3_ounp_inp_line(3 ,otype['prefix'])
+      replace_ww3_ounp_inp_line(7 ,otype['type'])
+      replace_ww3_ounp_inp_line(10,otype['subtype'])
+  
+      # Run the ww3_ounp program
+      subprocess.call(['srun','-n','4',pwd+'/ww3_ounp'])
+  
