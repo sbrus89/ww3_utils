8460569d2ce1a3bb4e93d382c5a0526cd3f895ac
Steven Brus <sbrus@lanl.gov>
2018-10-23 09:09:07 -0600

    Create configuration file for ww3_ounp program
     - Convert python module define_points.py to yaml config file
    
     - Rename files to conform to convention used with input file
       scripts

--- post_processing/ww3_ounp.py
+++ post_processing/ww3_ounp.py
@@ -1,9 +1,15 @@
-from define_points import *
 import os
+import yaml
+import pprint
+
 
 def write_ww3_ounp_inp():
   
   pwd = os.getcwd()
+
+  f = open(pwd+'/ww3_ounp.config')
+  cfg = yaml.load(f)
+  pprint.pprint(cfg)
   
   f = open(pwd+'/ww3_ounp.inp','w')
   
@@ -11,31 +17,31 @@ def write_ww3_ounp_inp():
   f.write('$ WAVEWATCH III NETCDF Point output post-processing                    $\n')
   f.write('$ -------------------------------------------------------------------- $\n')
   f.write('$\n')
-  f.write('   '+'  '.join([first_output_time,increment,number_of_outputs])+'\n')
+  f.write('   '+'  '.join([cfg['first_output_time'],cfg['increment'],cfg['number_of_outputs']])+'\n')
   f.write('$\n')
   f.write('$ Points requested --------------------------------------------------- $\n')
-  for point in points_requested:
+  for point in cfg['points_requested']:
     f.write('   '+point+'\n')
   f.write('$ mandatory end of list\n')
   f.write('-1\n')
   f.write('$\n')
   f.write('$ -------------------------------------------------------------------- $\n')
   f.write('$ file prefix\n')
-  f.write('   '+file_prefix+'\n')
+  f.write('   '+cfg['file_prefix']+'\n')
   f.write('$ number of characters in date\n')
-  f.write('   '+date_length+'\n')
+  f.write('   '+cfg['date_length']+'\n')
   f.write('$ NetCDF version\n')
-  f.write('   '+nc_version+'\n')
+  f.write('   '+cfg['nc_version']+'\n')
   f.write('$ points in same file, max number of points processed in one pass\n')
-  f.write('   '+'  '.join([points_in_same_file,max_points_processed])+'\n')
+  f.write('   '+'  '.join([cfg['points_in_same_file'],cfg['max_points_processed']])+'\n')
   f.write('$ output type\n')
-  f.write('   '+output_type+'\n')
+  f.write('   '+cfg['output_type']+'\n')
   f.write('$ flag for global attributes\n')
-  f.write('   '+global_attributes+'\n')
+  f.write('   '+cfg['global_attributes']+'\n')
   f.write('$ flag for dimensions order\n')
-  f.write('   '+dimensions_order+'\n')
+  f.write('   '+cfg['dimensions_order']+'\n')
   f.write('$ sub-type\n')
-  f.write('   '+sub_type+'\n')
+  f.write('   '+cfg['sub_type']+'\n')
   f.write('$ -------------------------------------------------------------------- $\n')
   f.write('$ End of input file                                                    $\n')
   f.write('$ -------------------------------------------------------------------- $\n')
