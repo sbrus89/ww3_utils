97705c72b92fb060b4cd0aead59a6d7e5e3f6e9d
Steven Brus <sbrus@lanl.gov>
2018-10-31 11:21:40 -0600

    Updates to plot_fields.py
     - Add ability to create difference plots between to solutions.
       One solution can be double or half the resolution of the other.

--- post_processing/plot_fields.py
+++ post_processing/plot_fields.py
@@ -11,6 +11,31 @@ import subprocess
 from mpl_toolkits.basemap import Basemap
 plt.switch_backend('agg')
 
+def read_field(f):
+
+  nc_file = netCDF4.Dataset(f,'r')
+
+  ref_date = nc_file.variables['time'].getncattr('units').replace('days since ','')
+  ref_date = datetime.datetime.strptime(ref_date,'%Y-%m-%d %H:%M:%S')
+  output_time = nc_file.variables['time'][:]
+
+  if len(output_time) > 1:
+    print "Should be only one timestep per nc file. Check ww3_ounf.inp"
+    raise SystemExit(0)
+
+  date = ref_date + datetime.timedelta(days=output_time[0])
+  output_date = date.strftime('%Y %m %d %H %M')
+
+  lon  = nc_file.variables['longitude'][:] 
+  lat  = nc_file.variables['latitude'][:]
+
+  var = nc_file.variables['hs'][0,:,:]
+
+  return lon,lat,var,output_date
+
+###############################################################################################
+###############################################################################################
+
 pwd = os.getcwd()
 
 # Read in configuration file
@@ -19,38 +44,56 @@ cfg = yaml.load(f)
 pprint.pprint(cfg)
 
 # Find nc file names
-output_direc = cfg['model_direc']['direc']
+runs = [x for x in cfg['model_direc']]
+nruns = len(runs)
+if nruns > 2:
+  print "Only 2 runs can be specified for difference plots"
+  raise SystemExit(0)
+output_direc = cfg['model_direc'][runs[0]]
 files = sorted(glob.glob(output_direc+'*.nc'))
 
+count = 0
 for f in files:
-  print f.split('/')[-1]
+  filename = f.split('/')[-1]
+  print filename
 
-  nc_file = netCDF4.Dataset(f,'r')
+  lon,lat,var,output_date = read_field(f)
+  if nruns > 1:
+    lon2,lat2,var2,output_date2 = read_field(cfg['model_direc'][runs[1]]+filename)
+    if var.shape == var2.shape:
+      var = np.absolute(var-var2)
+    elif (var.shape[0] == var2.shape[0]/2) and (var.shape[1] == var2.shape[1]/2):
+      var = np.absolute(var-var2[::2,::2])
+    elif (var.shape[0] == var2.shape[0]*2) and (var.shape[1] == var2.shape[1]*2):
+      var = np.absolute(var[::2,::2]-var2)
+      lon = lon2
+      lat = lat2
 
-  ref_date = nc_file.variables['time'].getncattr('units').replace('days since ','')
-  ref_date = datetime.datetime.strptime(ref_date,'%Y-%m-%d %H:%M:%S')
-  output_time = nc_file.variables['time'][:]
-
-  output_date = []
-  for t in output_time:
-    date = ref_date + datetime.timedelta(days=t)
-    output_date.append(date.strftime('%Y %m %d %H %M'))
-
-  lon  = nc_file.variables['longitude'][:] 
-  lat  = nc_file.variables['latitude'][:]
-
-  for i,t in enumerate(output_date): 
-    var  = nc_file.variables['hs'][i,:,:]
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    levels = np.linspace(0.0,10.0,100)
-    cf = ax.contourf(lon,lat,var,levels=levels)
-    ax.axis('image')
-    ax.set_title(t)
-    plt.colorbar(cf,ax=ax,orientation='horizontal')
-    plt.savefig('-'.join(t.split())+'.png')
-    plt.close() 
+  count = count + 1
+  if count == 1:
+    var_avg = np.copy(var)
+  else:
+    var_avg = var_avg + var
+      
+  fig = plt.figure()
+  ax = fig.add_subplot(1,1,1)
+  cf = ax.contourf(lon,lat,var,100)
+  ax.axis('image')
+  ax.set_title(output_date)
+  plt.colorbar(cf,ax=ax,orientation='horizontal')
+  plt.savefig('-'.join(output_date.split())+'.png')
+  plt.close() 
   
+var_avg = var_avg/float(count)
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+cf = ax.contourf(lon,lat,var_avg,100)
+ax.axis('image')
+ax.set_title('Average')
+plt.colorbar(cf,ax=ax,orientation='horizontal')
+plt.savefig('average.png')
+plt.close() 
+
 if not os.path.exists(cfg["plot_direc"]):
   subprocess.call(['mkdir','-p',cfg["plot_direc"]])
-  subprocess.call('mv *.png '+cfg["plot_direc"],shell=True)    
+subprocess.call('mv *.png '+cfg["plot_direc"],shell=True)    
