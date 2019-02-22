672cd74b57c49e672348a9c7752093da8ed2163b
Steven Brus <steven.r.brus@gmail.com>
2019-02-22 09:11:41 -0700

    Add checks in directional_spectrum.py
     - Make sure there are the same number of time snaps is the same for
       all variables
    
     - Make sure there are the same numver of frequency bins for all
       variables
    
     - Currently, this is setup for individual plot for a single station

--- stations/directional_spectrum.py
+++ stations/directional_spectrum.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.basemap import Basemap
 import pprint
 
+#mode = 'average'
+mode = 'individual'
 data_direc = 'spectral_data/'
 year = '2005'
 
@@ -14,8 +18,10 @@ def read_station_data(sta,year,variables):
 
   obs_data = {}
   for i,var in enumerate(variables):
-    print '  '+var
+    print '  reading'+var
 
+    success = True
+ 
     # Open station data file
     f = open(data_direc+sta+'_'+year+'_'+var+'.txt','r')
     obs = f.read().splitlines()
@@ -23,7 +29,7 @@ def read_station_data(sta,year,variables):
     # Get the fequency data and initialize time list
     obs_data[var] = []
     if i == 0:
-      obs_data['freq'] = obs[0].split()[5:]
+      obs_data['freq'] = obs[0].strip().split()[5:]
       obs_data['freq'] = np.asarray(obs_data['freq'])
       obs_data['freq'] = obs_data['freq'].astype(np.float)
       obs_data['datetime'] = []
@@ -37,46 +43,107 @@ def read_station_data(sta,year,variables):
         obs_data['datetime'].append(' '.join(line_sp[0:5]))
       obs_data[var].append(line_sp[5:])
       if len(line_sp[5:]) != nfreq:
-        print '  dimension error in line '+str(j)
-  
+        success = False
+ 
     # Convert observation data
     obs_data[var] = np.asarray(obs_data[var],dtype=np.float)            
     
   
-  return obs_data
+  return obs_data,success
 
 #######################################################################
 #######################################################################
 
-def plot_station_spectrum(sta,obs_data):
+def compute_spectrum(obs_data, mode='average'):
 
+  print '  computing spectrum'
+
+  success = True
 
   nsnaps = len(obs_data['datetime'])
+  if obs_data['swden'].shape[0] != nsnaps or obs_data['swdir'].shape[0] != nsnaps or obs_data['swdir2'].shape[0] != nsnaps or obs_data['swr1'].shape[0] != nsnaps or obs_data['swr2'].shape[0] != nsnaps:
+    success = False
 
-  theta = np.radians(np.linspace(0.0,360.0,37))[np.newaxis].T
-  print theta.shape, obs_data['freq'].shape
-  n = obs_data['freq'].size
-  R,Theta = np.meshgrid(obs_data['freq'],theta)
+  nfreq = obs_data['freq'].size
+  ndir = 37
+  theta = np.radians(np.linspace(0.0,360.0,ndir))[np.newaxis].T
+  obs_data['theta'] = theta
+
+  if mode == 'average':
+    spectrum = np.zeros((1,ndir,nfreq))
+  elif mode == 'individual':
+    spectrum = np.zeros((nsnaps,ndir,nfreq))
+ 
+  if success == False:
+    return spectrum, success
 
   for i in range(nsnaps):
-    C11    = obs_data['swden'][i,0:n]
-    alpha1 = np.radians(obs_data['swdir'][i,0:n])
-    alpha2 = np.radians(obs_data['swdir2'][i,0:n])
-    r1     = obs_data['swr1'][i,0:n]*0.01
-    r2     = obs_data['swr2'][i,0:n]*0.01
+    C11    = obs_data['swden'][i,0:nfreq]
+    alpha1 = np.radians(obs_data['swdir'][i,0:nfreq])
+    alpha2 = np.radians(obs_data['swdir2'][i,0:nfreq])
+    r1     = obs_data['swr1'][i,0:nfreq]*0.01
+    r2     = obs_data['swr2'][i,0:nfreq]*0.01
+ 
+    spec = C11*(0.5 + r1*np.cos(theta-alpha1) + r2*np.cos(2.0*(theta-alpha2)))/np.pi
 
-    spectrum = C11*(0.5 + r1*np.cos(theta-alpha1) + r2*np.cos(2.0*(theta-alpha2)))/np.pi
+    if mode == 'average':
+      spectrum[0,:,:] = spectrum[0,:,:] + spec 
+    elif mode == 'individual':
+      spectrum[i,:,:] = spec 
+    
+  if mode == 'average':
+    spectrum = spectrum/float(nsnaps)
 
-    fig,ax = plt.subplots(subplot_kw=dict(projection='polar'))
-    ax.set_theta_zero_location("N")
-    ax.set_theta_direction(-1)
-    cax = ax.contourf(Theta,R,spectrum,30)
-    cb = fig.colorbar(cax)
-    fig.tight_layout()
-    fig.savefig(sta+'_'+str(i)+'.png',bbox_inches='tight')
+  return spectrum, success
 
-    if i > 15:
-      break
+
+#######################################################################
+#######################################################################
+
+def plot_station_spectrum(sta,lon,lat,dates,freq,theta,obs_data):
+
+  print '  plotting spectrum'
+
+  nsnaps = len(dates)
+
+  R,Theta = np.meshgrid(freq,theta)
+
+  for i in range(nsnaps):
+
+    if i % 20 == 0:
+
+      fig = plt.figure(figsize=[6,8])
+      gs = gridspec.GridSpec(nrows=2,ncols=2,figure=fig)
+
+      # Plot global station location
+      ax = fig.add_subplot(gs[0,0])
+      m = Basemap(projection='cyl',llcrnrlat= -90,urcrnrlat=90,\
+                                   llcrnrlon=-180,urcrnrlon=180,resolution='c')
+      m.fillcontinents(color='tan',lake_color='lightblue')
+      m.drawcoastlines()
+      ax.plot(lon,lat,'ro')
+
+      # Plot local station location
+      ax = fig.add_subplot(gs[0,1])
+      m = Basemap(projection='cyl',llcrnrlat=lat-7.0 ,urcrnrlat=lat+7.0,\
+                                   llcrnrlon=lon-10.0,urcrnrlon=lon+10.0,resolution='l')
+      m.fillcontinents(color='tan',lake_color='lightblue')
+      m.drawcoastlines()
+      ax.plot(lon,lat,'ro')
+
+      ax = fig.add_subplot(gs[1,:],polar=True)
+
+      #fig,ax = plt.subplots(subplot_kw=dict(projection='polar'))
+      ax.set_theta_zero_location("N")
+      ax.set_theta_direction(-1)
+      cax = ax.contourf(Theta,R,spectrum[i,:,:],30)
+      cb = fig.colorbar(cax)
+      ax.set_title('Directional spectrum '+dates[i])
+      st = plt.suptitle('Station '+sta,fontsize=16)
+      fig.tight_layout()
+      fig.savefig(sta+'_'+str(i)+'.png',bbox_inches='tight',bbox_extra_artists=(st,))
+
+      plt.close()
 
 
 #######################################################################
@@ -94,13 +161,21 @@ if __name__ == '__main__':
 
     # Find station ID
     sta = line.split('  ')[2].replace("'","")
-    if sta != '42001':
-      continue 
-
+    lon = float(line.split('  ')[0])
+    lat = float(line.split('  ')[1])
     print sta
+
+    if sta != '46042':
+      continue
   
-    obs_data = read_station_data(sta,year,variables)
+    obs_data,success = read_station_data(sta,year,variables)
+    
+    if success:
+      spectrum, success = compute_spectrum(obs_data,mode)
   
-    plot_station_spectrum(sta,obs_data)  
-      
-    break
+    if success:
+      if mode == 'average':
+        plot_station_spectrum(sta,lon,lat,['averaged over '+year],obs_data['freq'],obs_data['theta'],spectrum)  
+      elif mode == 'individual':
+        plot_station_spectrum(sta,lon,lat,obs_data['datetime'],obs_data['freq'],obs_data['theta'],spectrum)  
+        
