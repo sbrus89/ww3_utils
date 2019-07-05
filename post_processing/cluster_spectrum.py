8defcdfd641190ae7376dc231c6b87fdb2201c39
Steven Brus <sbrus@lanl.gov>
2019-07-05 16:55:53 -0600

    Improvements to cluster_spectrum.py
     - Improve plots and colorscales
    
     - Add new clustering algorithms

--- post_processing/cluster_spectrum.py
+++ post_processing/cluster_spectrum.py
@@ -2,20 +2,31 @@ import glob
 import sys
 import numpy as np
 from matplotlib import cm
+from matplotlib.transforms import Bbox
+from matplotlib.patches import Rectangle
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from scipy import interpolate
 import directional_spectrum
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN 
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.mixture import GaussianMixture
+from sklearn.decomposition import PCA
 plt.switch_backend('agg')
 np.set_printoptions(threshold=sys.maxsize)
 
 stations_direc = '../../../../../'
 run_direc = '../../model_data/spectrum/'
 
+method = 'mixture'
+
+
 if __name__ == '__main__':
 
-  k = 36 
+  k = 25 
+
 
   stations = directional_spectrum.read_station_file(stations_direc+'stations_grid.txt')
 
@@ -32,25 +43,49 @@ if __name__ == '__main__':
   spec = np.reshape(run_data['spectrum'],(nstations,nfrequency*ndirection))
   print spec.shape
   
-  kmeans = KMeans(n_clusters=k,random_state=0).fit(spec)
+  if method == 'kmeans':
+    clustered = KMeans(n_clusters=k,random_state=0).fit(spec)
+    spec = clustered.cluster_centers_
+    labels = clustered.labels_
+  elif method == 'pca':
+    clustered = PCA(n_components=k).fit(spec)
+    spec = clustered.components_
+  elif method == 'dbscan':
+    clustered = DBSCAN(eps=1e-1,metric='l2',min_samples=100).fit(spec)
+    spec = clustered.components_
+    labels = clusterd.labels_
+  elif method == 'mixture':
+    clustered = GaussianMixture(n_components=k).fit(spec)
+    spec = clustered.means_
+    labels = clustered.predict(spec)
 
-  cmap = cm.get_cmap('gist_rainbow',k)
-  fig = plt.figure()
-  ax = fig.add_subplot()
-  m = Basemap(projection='cyl',llcrnrlat= -90,urcrnrlat=90,\
-                               llcrnrlon=-180,urcrnrlon=180,resolution='c')
-  m.fillcontinents(color='tan',lake_color='lightblue')
-  m.drawcoastlines()
-  plt.scatter(run_stations['lon'],run_stations['lat'],c=kmeans.labels_,cmap=cmap)
-  fig.tight_layout()
-  fig.savefig('clustered_spectrum.png',bbox_inches='tight')
-  plt.close()
+    
+  print spec.shape
+  k = spec.shape[0]
 
-  fig = plt.figure()
-  x = np.arange(k)
-  plt.scatter(x,x,c=x,cmap=cmap)
-  fig.savefig('colors.png',bbox_inches='tight')
-  plt.close()
+  cmap1 = cm.get_cmap('tab20b')(range(20))
+  cmap2 = cm.get_cmap('tab20c')(range(20))
+  cmap = ListedColormap(np.vstack((cmap1,cmap2)))
+  cmap = ListedColormap(cmap(range(k)))
+
+  if method == 'kmeans' or method == 'dbscan':
+
+    fig = plt.figure(figsize=[18.0,6.0])
+    ax = fig.add_subplot()
+    m = Basemap(projection='cyl',llcrnrlat= -90,urcrnrlat=90,\
+                                 llcrnrlon=-180,urcrnrlon=180,resolution='c')
+    m.fillcontinents(color='tan',lake_color='lightblue')
+    m.drawcoastlines()
+    plt.scatter(run_stations['lon'],run_stations['lat'],c=labels,cmap=cmap)
+    fig.tight_layout()
+    fig.savefig('clustered_spectrum.png',bbox_inches='tight')
+    plt.close()
+
+    fig = plt.figure()
+    x = np.arange(k)
+    plt.scatter(x,x,c=x,cmap=cmap)
+    fig.savefig('colors.png',bbox_inches='tight')
+    plt.close()
 
 
   theta2 = run_data['theta']
@@ -62,10 +97,8 @@ if __name__ == '__main__':
   freq1 = np.linspace(0.0,0.5,nfreq)
   R1,Theta1 = np.meshgrid(freq1,theta1)
 
-  spec = kmeans.cluster_centers_
-  print spec.shape
 
-  n = int(np.sqrt(k))
+  n = int(np.ceil(np.sqrt(k)))
 
   fig = plt.figure(figsize=[18,18])
   for i in range(k):
@@ -97,5 +130,15 @@ if __name__ == '__main__':
     ax.set_theta_direction(-1)
     cax = ax.contourf(Theta1,R1,spec2,30)
     cb = fig.colorbar(cax)
+
+
+  fig.tight_layout()
+  h = 1.0/float(n)
+  c = 0
+  for j in range(n):
+    for i in range(n):
+     rect = [Rectangle([0.0+(i*h),1.0-(j*h)-h],h,h,facecolor=cmap(c),edgecolor='none',zorder=-1,transform=fig.transFigure)]
+     c = c +1
+     fig.patches.extend(rect)
   
   fig.savefig('k_spectra.png',bbox_inches='tight')
