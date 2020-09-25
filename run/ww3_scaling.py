434c99bf61bc5f76e0828ccadae0dc33c4b7f5a8
Steven Brus <sbrus@lanl.gov>
2020-09-25 14:52:56 -0600

    Improve ww3_scaling.py

--- run/ww3_scaling.py
+++ run/ww3_scaling.py
@@ -4,7 +4,9 @@ import submission_script
 import os
 
 pes = [36,72,144,288,576,1152,1800]
-time = 4.0
+time = 16.0
+queue = 'standard'
+#queue = 'interactive'
 
 if __name__ == '__main__':
 
@@ -12,19 +14,19 @@ if __name__ == '__main__':
 
 
   for i,np in enumerate(pes):
+     
+    # Create run directory
+    np_dir = 'np'+str(np)
+    subprocess.call('mkdir '+np_dir,shell=True)
+    os.chdir(np_dir)
+    ln_files = ['wind.ww3','ww3_shel.inp','mod_def.ww3','*.in','restart.ww3','ww3_shel']
+    for f in ln_files:
+      subprocess.call('ln -sf ../'+f+' .',shell=True)
 
     # Create information for submission script
     job_name = 'ww3_np'+str(np)
     sub_file = job_name+'.sub'
-    np_dir = 'np'+str(np)
-    post_cmds = ['mkdir '+np_dir,
-                 'mv '+job_name+'.o* '+np_dir,
-                 'mv '+job_name+'.e* '+np_dir]
- 
-    if np > 2528:
-      queue = 'standard'
-    else:
-      queue = 'interactive'
+    pre_cmds = ['cd '+np_dir]
 
     # Write the submission script
     submission_script.write_submission_script(machine='grizzly',
@@ -34,19 +36,16 @@ if __name__ == '__main__':
                                               queue=queue,
                                               exe='ww3_shel',
                                               filename=sub_file,
-                                              post_cmds=post_cmds)
+                                              pre_cmds=pre_cmds)
+    if i > 0:
+      time = time/2.0
 
-    # Submit the runs with depenencies
-    if os.path.isfile(pwd+'/ww3_shel'):
-      if i > 0:
-        run_cmd = ['sbatch','--dependency=afterok:'+job_id,sub_file]
-      else:
-        run_cmd = ['sbatch',sub_file]
+    run_cmd = ['sbatch',sub_file]
+    print(' '.join(run_cmd))
+    output = subprocess.Popen(run_cmd, stdout=subprocess.PIPE).communicate()[0]
+    print(output)
 
-      print ' '.join(run_cmd)
-      output = subprocess.Popen(run_cmd, stdout=subprocess.PIPE).communicate()[0]
-      print output
+    os.chdir('..')
 
-      job_id = output.split()[-1]
 
     
