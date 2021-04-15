4151465553c13d2ebd5e61c8ccff06e222b1594e
Steven Brus <steven.r.brus@gmail.com>
2021-04-15 15:50:35 -0500

    Add CFSR 0.5x0.5 degree grid to CFSR_SCRIP_file.py

--- grid/CFSR_SCRIP_file.py
+++ grid/CFSR_SCRIP_file.py
@@ -4,16 +4,24 @@ import subprocess
 #nlat = 94
 #nlon = 192
 #outfile = 'T62.SCRIP.nc'
+#lat_typ = 'gss'
 
-title = 'CFSv2 0.205x0.204 grid'
-nlat = 880 
-nlon = 1760
-outfile = 'CFSv2.SCRIP.nc'
+#title = 'CFSv2 0.205x0.204 grid'
+#nlat = 880 
+#nlon = 1760
+#outfile = 'CFSv2.SCRIP.nc'
+#lat_typ = 'gss'
+
+title = 'CFSR 0.5x0.5 grid'
+nlat = 361 
+nlon = 720
+outfile = 'CFSR.SCRIP.nc'
+lat_typ = 'cap'
 
 cmd = 'ncremap -G '
 cmd = cmd + "ttl='"+title+"'"
 cmd = cmd + '#latlon='+str(nlat)+','+str(nlon)
-cmd = cmd + '#lat_typ=gss'
+cmd = cmd + '#lat_typ='+lat_typ
 cmd = cmd + '#lon_typ=grn_ctr'
 cmd = cmd + ' -g '+outfile
 
