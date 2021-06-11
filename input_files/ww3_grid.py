2882ef44a44240a9d7e92420a233249b892b1595
Steven Brus <sbrus@lanl.gov>
2021-06-11 07:35:57 -0600

    Modify ww3_grid.py to handle unstructured grids

--- input_files/ww3_grid.py
+++ input_files/ww3_grid.py
@@ -7,8 +7,12 @@ import pprint
 def write_ww3_grid_inp():
   
   pwd = os.getcwd()
-
-  f = open(pwd+'/ww3_grid.config') 
+ 
+  filenames = ['ww3_grid.config','ww3_grid.config.struc','ww3_grid.config.unst']
+  for fname in filenames:
+    if os.path.exists(fname):
+      f = open(pwd+'/'+fname) 
+      break
   cfg = yaml.load(f)
   pprint.pprint(cfg)
   
@@ -76,14 +80,15 @@ def write_ww3_grid_inp():
   f.write('   '+' '.join([cfg["gstrg"], 
                           cfg["flagll"], 
                           cfg["cstrg"]]) + '\n')
-  f.write('   '+'  '.join([cfg["nx"], 
-                           cfg["ny"]]) + '\n')
-  f.write('   '+'  '.join([cfg["sx"], 
-                           cfg["sy"], 
-                           cfg["grd_sca_fac"]]) + '\n')
-  f.write('   '+'  '.join([cfg["x11"], 
-                           cfg["y11"], 
-                           cfg["crd_sca_fac"]]) + '\n')
+  if cfg["gstrg"] != "'UNST'":
+    f.write('   '+'  '.join([cfg["nx"], 
+                             cfg["ny"]]) + '\n')
+    f.write('   '+'  '.join([cfg["sx"], 
+                             cfg["sy"], 
+                             cfg["grd_sca_fac"]]) + '\n')
+    f.write('   '+'  '.join([cfg["x11"], 
+                             cfg["y11"], 
+                             cfg["crd_sca_fac"]]) + '\n')
 
   #-------------------
   # Bottom depth file
@@ -102,25 +107,39 @@ def write_ww3_grid_inp():
   #------------------
   # Obstruction file
   #------------------
-  f.write('$ Sub-grid information\n')
-  f.write('   '+'  '.join([cfg["obs_unit"],
-                           cfg["obs_sca_fac"], 
-                           cfg["obs_idla"], 
-                           cfg["obs_idfm"], 
-                           cfg["obs_frmt"], 
-                           cfg["obs_from"], 
-                           cfg["obs_name"]]) + '\n')
+  if cfg["gstrg"] != "'UNST'":
+    f.write('$ Sub-grid information\n')
+    f.write('   '+'  '.join([cfg["obs_unit"],
+                             cfg["obs_sca_fac"], 
+                             cfg["obs_idla"], 
+                             cfg["obs_idfm"], 
+                             cfg["obs_frmt"], 
+                             cfg["obs_from"], 
+                             cfg["obs_name"]]) + '\n')
 
   #-----------
   # Mask file
   #-----------
-  f.write('$ Mask information\n')
-  f.write('   '+'  '.join([cfg["msk_unit"], 
-                           cfg["msk_idla"],
-                           cfg["msk_idfm"],
-                           cfg["msk_frmt"],
-                           cfg["msk_from"],
-                           cfg["msk_name"]]) + '\n')
+  if cfg["gstrg"] != "'UNST'":
+    f.write('$ Mask information\n')
+    f.write('   '+'  '.join([cfg["msk_unit"], 
+                             cfg["msk_idla"],
+                             cfg["msk_idfm"],
+                             cfg["msk_frmt"],
+                             cfg["msk_from"],
+                             cfg["msk_name"]]) + '\n')
+
+  #-----------------------
+  # Input boundary points
+  #-----------------------
+  if cfg["gstrg"] == "'UNST'":
+    f.write('$ Input boundary points\n')
+    f.write('   '+"10 3 1 '(...)' 'PART' 'mapsta.inp'\n")
+    f.write('   '+'0   0   F\n') 
+    f.write('   '+'0   0   F\n') 
+    f.write('   '+'0   0\n') 
+
+
   f.write('$\n')
   f.write('   '+4*'0.   '+'0 \n')
   f.write('$ -------------------------------------------------------------------- $\n')
