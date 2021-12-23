b7ad67c2c8b56e3f4dd11a07dc035a7a52139a3c
Steven Brus <steven.r.brus@gmail.com>
2021-12-23 15:15:27 -0600

    Use process_output.py in setup_postprocessing_direc.py

--- post_processing/setup_postprocessing_direc.py
+++ post_processing/setup_postprocessing_direc.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-run_name = '20210930.UWAV-CFSR.CFSR_wQU225EC60to30.anvil'
+run_name = '20211221.AWAV_WCYCLSSP585_CMIP6.ne30_ECv3_wQU225EC60to30.anvil'
 post_processing_dir = './post_processing'
 obs_dir = '/home/sbrus/run/WW3_testing/glo_30m/post_processing/obs_data/'
 output_dir = '/lcrc/group/acme/sbrus/scratch/anvil/'+run_name+'/run/'
@@ -37,15 +37,13 @@ def replace_config_file_line(direc,config_file,option,replacement):
 commands = [['mkdir','-p',post_processing_dir],
             ['ln', '-srf', ww3_utils+'ww3_ounp.py',            post_processing_dir],
             ['ln', '-srf', ww3_utils+'ww3_ounf.py',            post_processing_dir],
-            ['ln', '-srf', ww3_utils+'process_points.py',      post_processing_dir],
-            ['ln', '-srf', ww3_utils+'process_fields.py',      post_processing_dir],
+            ['ln', '-srf', ww3_utils+'process_output.py',      post_processing_dir],
             ['ln', '-srf', ww3_utils+'plot_points.py',         post_processing_dir],
             ['ln', '-srf', ww3_utils+'plot_fields.py',         post_processing_dir],
             ['ln', '-srf', ww3_utils+'write_vtk.py',           post_processing_dir],
             ['cp',         ww3_utils+'ww3_ounp.config',        post_processing_dir],
             ['cp',         ww3_utils+'ww3_ounf.config',        post_processing_dir],
-            ['cp',         ww3_utils+'process_points.config',  post_processing_dir],
-            ['cp',         ww3_utils+'process_fields.config',  post_processing_dir],
+            ['cp',         ww3_utils+'process_output.config',  post_processing_dir],
             ['cp',         ww3_utils+'plot_points.config',     post_processing_dir],
             ['cp',         ww3_utils+'plot_fields.config',     post_processing_dir],
             ['cp',         ww3_utils+'write_vtk.config',       post_processing_dir],
@@ -58,14 +56,10 @@ for cmd in commands:
   print(' '.join(cmd))
   subprocess.call(cmd)
 
-replace_config_file_line(post_processing_dir,'process_points.config','output_direc',output_dir)
-replace_config_file_line(post_processing_dir,'process_fields.config','output_direc',output_dir)
-replace_config_file_line(post_processing_dir,'process_points.config','run_direc',run_dir)
-replace_config_file_line(post_processing_dir,'process_fields.config','run_direc',run_dir)
-replace_config_file_line(post_processing_dir,'process_points.config','log_direc',log_dir)
-replace_config_file_line(post_processing_dir,'process_fields.config','log_direc',log_dir)
-replace_config_file_line(post_processing_dir,'process_points.config','data_direc',nc_output_dir+'points/')
-replace_config_file_line(post_processing_dir,'process_fields.config','data_direc',nc_output_dir+'fields/')
+replace_config_file_line(post_processing_dir,'process_output.config','output_direc',output_dir)
+replace_config_file_line(post_processing_dir,'process_output.config','run_direc',run_dir)
+replace_config_file_line(post_processing_dir,'process_output.config','log_direc',log_dir)
+replace_config_file_line(post_processing_dir,'process_output.config','data_direc',nc_output_dir+'points/')
 os.chdir(post_processing_dir)
 subprocess.call(['python', 'ww3_ounf.py'])
 subprocess.call(['python', 'ww3_ounp.py'])
